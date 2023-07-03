d55febd91f92571d944a30fe82e0efa0a3c17fc6
Виктор <vsolodovnik88@mail.ru>
2023-07-03 23:32:56 +0300

    route-2

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -21,6 +21,47 @@ class Router
     }
     public static function dispatch($url)
     {
-        var_dump($url);
+        if (self::matchRoute($url)) {
+            echo 'oK';
+        } else {
+            echo 'NO';
+        }
+    }
+    public static function matchRoute($url): bool
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#i", $url, $matches)) {
+                foreach ($matches as $key => $value) {
+                    if (is_string($key)) {
+                        $route[$key] = $value;
+                    }
+                }
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+                debug($route);
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                debug($route);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected static function upperCamelCase($name): string
+    {
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
+    protected static function lowerCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
     }
 }
