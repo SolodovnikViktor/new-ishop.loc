4b62c892dfe7c7eba58077857b524eab56195f20
Виктор <vsolodovnik88@mail.ru>
2023-07-03 22:48:00 +0300

    Router

--- public/index.php
+++ public/index.php
@@ -4,5 +4,7 @@ if (PHP_MAJOR_VERSION < 8) {
     die('Установите PHP 8.0 или выше');
 }
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
